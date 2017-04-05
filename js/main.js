cef75c67129fcd7a3416313b68a55f1b115cf6ae
jsap1904 <sapinoso.j@gmail.com>
2017-04-04 20:43:52 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,66 +1,88 @@
-var honda = { 
-	car: "s2000",
-	hp: 220,
-	year: 2005
-	}
-var nissan = {
-	car:"350z"
-	hp: 300,
-	year: 2008
-}	
-var subaru = {
-	car: "STI",
-	hp: 300,
-	year: 2013
-}
+// var honda = { 
+// 	car: "s2000",
+// 	hp: 220,
+// 	year: 2005
+// 	}
+// var nissan = {
+// 	car:"350z"
+// 	hp: 300,
+// 	year: 2008
+// }	
+// var subaru = {
+// 	car: "STI",
+// 	hp: 300,
+// 	year: 2013
+// }
 
-function vehicles(car, hp, year) {
-	this.car = car
-	this.hp = hp
-	this.year = year
-}
+// function vehicles(car, hp, year) {
+// 	this.car = car
+// 	this.hp = hp
+// 	this.year = year
+// }
 
-var honda = new vehicles("s2000", 220, 2005)
-var nissan = new vehicles("350z", 300, 2008)
-var subaru = new vehicles("STI", 300, 2013)
-//write a function in object that calculates something within the object and displays it
-function restaurants (name,seats,bookedseats,seatsOpen) {
-	this.name = name
-	this.seats = seats
-	this.bookedseats = bookedseats
-	this.seatsAvailable = function() {
-		return seats - bookedseats
-	}
-	this.seatsOpen = this.seatsAvailable()
-}
+// var honda = new vehicles("s2000", 220, 2005)
+// var nissan = new vehicles("350z", 300, 2008)
+// var subaru = new vehicles("STI", 300, 2013)
+// //write a function in object that calculates something within the object and displays it
+// function restaurants (name,seats,bookedseats,seatsOpen) {
+// 	this.name = name
+// 	this.seats = seats
+// 	this.bookedseats = bookedseats
+// 	this.seatsAvailable = function() {
+// 		return seats - bookedseats
+// 	}
+// 	this.seatsOpen = this.seatsAvailable()
+// }
 
-var burrito = new restaurants("Kanye Asada", 20, 10)
-var pizza = new restaurants("Project Pie", 20, 10)
-var burger = new restaurants("Super Duper", 20, 10)
+// var burrito = new restaurants("Kanye Asada", 20, 10)
+// var pizza = new restaurants("Project Pie", 20, 10)
+// var burger = new restaurants("Super Duper", 20, 10)
 
 
 
-console.log(burrito, pizza, burger)
+// console.log(burrito, pizza, burger)
 
-//displaying calculation of 1+2+3+4+5
-var userInput = number( prompt("type in a number 1-10") )
-var previousNum = previousNum + i
+// //displaying calculation of 1+2+3+4+5
+// var userInput = number( prompt("type in a number 1-10") )
+// var previousNum = previousNum + i
 
-for( var i =userInput; i < 15; i++) {
-	previousNum = previousNum + i
-	console.log(previousNum)
+// for( var i =userInput; i < 15; i++) {
+// 	previousNum = previousNum + i
+// 	console.log(previousNum)
 
-}
-//multiplication table
-for( var i = 1; i <=12 ; i++) {
-	for (var k = 1; k <= 12; k++) {
-		console.log(i*k)
-	}
- }
+// }
+// //multiplication table
+// for( var i = 1; i <=12 ; i++) {
+// 	for (var k = 1; k <= 12; k++) {
+// 		console.log(i*k)
+// 	}
+//  }
 
-var num = 1.2
+// var num = 1.2
 
-console.log(Math.ceil(num)) //brings # up
-console.log(Math.floor(num)) // bring # down
-console.log(Math.round(num)) //rounds to nearest whole number
-console.log(Math.random() * 2) // will give you either 1 or 0 always
+// console.log(Math.ceil(num)) //brings # up
+// console.log(Math.floor(num)) // bring # down
+// console.log(Math.round(num)) //rounds to nearest whole number
+// console.log(Math.random() * 2) // will give you either 1 or 0 always
+
+// var today = new Date()
+
+
+// 	console.log(today.getDate())
+// 	console.log(today.getDay())
+// 	console.log(today.getMonth())
+// 	console.log(today.getFullYear())
+
+
+// setTimeout(function(){
+// 		alert("hello")
+// },2000)
+
+var count = 0
+
+setInterval(function(){
+		console.log("hello")
+		count ++
+		console.log(count)
+},2000)
+
